610adf267a46e5edb8aa940498cb799573457fb2
anasyusef <anas.y0807@gmail.com>
2021-07-12 00:45:34 +0100

    refactor: change from fargate to ecs-ec2

--- main.tf
+++ main.tf
@@ -13,12 +13,100 @@ provider "aws" {
   region = var.region
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "freqtrade_vpc"
+  cidr   = "10.0.0.0/16"
+
+  azs = [
+    "ap-northeast-1a",
+    "ap-northeast-1c",
+    "ap-northeast-1d"
+  ]
+  public_subnets = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+}
+
+resource "aws_security_group" "ec2_instances_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress = [
+    # {
+    # cidr_blocks      = ["0.0.0.0/0"]
+    # description      = "Allow SSH from anywhere (develop)"
+    # from_port        = 22
+    # ipv6_cidr_blocks = []
+    # prefix_list_ids  = []
+    # protocol         = "TCP"
+    # security_groups  = []
+    # self             = false
+    # to_port          = 22
+    # },
+    {
+      cidr_blocks      = []
+      description      = "Allow inbound traffic from ALB"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "all"
+      security_groups  = ["${aws_security_group.load_balancer_sg.id}"]
+      self             = false
+      to_port          = 0
+  }]
+
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allow every outbound traffic"
+    from_port        = 0
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    protocol         = -1
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+
+  depends_on = [
+    aws_security_group.load_balancer_sg
+  ]
+}
+
+
+module "ec2_instances" {
+  source         = "terraform-aws-modules/ec2-instance/aws"
+  name           = "freqtrade-ec2-cluster"
+  instance_count = 1
+
+  ami                    = "ami-0f4146903324aaa5b"
+  instance_type          = "t2.micro"
+  key_name               = "TokyoKey"
+  vpc_security_group_ids = [aws_security_group.ec2_instances_sg.id]
+  iam_instance_profile   = "ecsInstanceRole"
+  subnet_ids             = module.vpc.public_subnets
+  user_data              = <<EOT
+  #!/bin/bash
+  echo ECS_CLUSTER="${aws_ecs_cluster.freqtrade_cluster.name}" >> /etc/ecs/ecs.config
+  EOT
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+  depends_on = [
+    aws_ecs_cluster.freqtrade_cluster
+  ]
+}
+
 resource "aws_ecr_repository" "freqtrade_bot" {
   name = "freqtrade_bot"
 }
 
 resource "aws_ecs_cluster" "freqtrade_cluster" {
   name = "freqtrade_cluster"
+
 
   setting {
     name  = "containerInsights"
@@ -29,22 +117,16 @@ resource "aws_ecs_cluster" "freqtrade_cluster" {
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
-
-# data "aws_ecr_image" "freqtrade_bot" {
-#   repository_name = aws_ecr_repository.freqtrade_bot.name
-# }
-
 resource "aws_ecs_task_definition" "freqtrade_task" {
   family = "freqtrade_task"
   container_definitions = jsonencode(
     [
       {
         name      = "freqtrade_task"
-        # image     = "${aws_ecr_repository.freqtrade_bot.repository_url}:latest@${data.aws_ecr_image.freqtrade_bot.id}"
         image     = "${aws_ecr_repository.freqtrade_bot.repository_url}:latest"
         essential = true
-        memory    = 1024
-        cpu       = 512
+        # memory    = 1024
+        # cpu       = 512
         logConfiguration = {
           logDriver = "awslogs"
           options = {
@@ -68,10 +150,9 @@ resource "aws_ecs_task_definition" "freqtrade_task" {
         ]
       }
   ])
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  memory                   = "1024"
-  cpu                      = "512"
+  requires_compatibilities = ["EC2"]
+  memory                   = "400"
+  cpu                      = "400"
   execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
 }
@@ -81,87 +162,72 @@ resource "aws_cloudwatch_log_group" "freqtrade_log_group" {
   retention_in_days = 7
 }
 resource "aws_ecs_service" "freqtrade_service" {
-  name                              = "freqtrade_service"
-  cluster                           = aws_ecs_cluster.freqtrade_cluster.id
-  task_definition                   = aws_ecs_task_definition.freqtrade_task.arn
-  launch_type                       = "FARGATE"
-  desired_count                     = 1
-  health_check_grace_period_seconds = 120
+  name                               = "freqtrade_service"
+  cluster                            = aws_ecs_cluster.freqtrade_cluster.id
+  task_definition                    = aws_ecs_task_definition.freqtrade_task.arn
+  launch_type                        = "EC2"
+  desired_count                      = 1
+  health_check_grace_period_seconds  = 120
   deployment_minimum_healthy_percent = 0
-  deployment_maximum_percent = 100
+  deployment_maximum_percent         = 100
 
-  network_configuration {
-    subnets = [
-      "${aws_default_subnet.default_subnet_a.id}",
-      "${aws_default_subnet.default_subnet_b.id}",
-      "${aws_default_subnet.default_subnet_c.id}"
-    ]
-    assign_public_ip = true
-    security_groups  = ["${aws_security_group.service_sg.id}"]
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
   }
+
+  # network_configuration {
+  #   subnets = module.vpc.public_subnets
+  #   security_groups  = ["${aws_security_group.service_sg.id}"]
+  # }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn
     container_name   = aws_ecs_task_definition.freqtrade_task.family
     container_port   = 8080
   }
+
+  depends_on = [
+    aws_lb.application_load_balancer
+  ]
 }
 
-resource "aws_security_group" "service_sg" {
+# resource "aws_security_group" "service_sg" {
+#   vpc_id = module.vpc.vpc_id
+#   ingress = [{
+#     cidr_blocks      = []
+#     description      = "Only allow traffic from the load balancer"
+#     from_port        = 0
+#     ipv6_cidr_blocks = []
+#     prefix_list_ids  = []
+#     protocol         = "all"
+#     security_groups  = ["${aws_security_group.load_balancer_sg.id}"]
+#     self             = false
+#     to_port          = 0
+#   }]
 
-  ingress = [{
-    cidr_blocks      = []
-    description      = "Only allow traffic from the load balancer"
-    from_port        = 0
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "-1"
-    security_groups  = ["${aws_security_group.load_balancer_sg.id}"]
-    self             = false
-    to_port          = 0
-  }]
-
-  egress = [{
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "Allow outbund traffic to anywhere"
-    from_port        = 0
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "-1"
-    security_groups  = []
-    self             = false
-    to_port          = 0
-  }]
-}
-
-resource "aws_default_vpc" "default_vpc" {
-}
-
-resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = "${var.region}a"
-}
-
-resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = "${var.region}d"
-}
-
-resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = "${var.region}c"
-}
-
-resource "aws_alb" "application_load_balancer" {
+#   egress = [{
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     description      = "Allow outbund traffic to anywhere"
+#     from_port        = 0
+#     ipv6_cidr_blocks = []
+#     prefix_list_ids  = []
+#     protocol         = "all"
+#     security_groups  = []
+#     self             = false
+#     to_port          = 0
+#   }]
+# }
+resource "aws_lb" "application_load_balancer" {
   name               = "freqtrade-lb"
   internal           = false
   load_balancer_type = "application"
-  subnets = [
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}",
-    "${aws_default_subnet.default_subnet_c.id}"
-  ]
-  security_groups = ["${aws_security_group.load_balancer_sg.id}"]
+  subnets            = module.vpc.public_subnets
+  security_groups    = ["${aws_security_group.load_balancer_sg.id}"]
 }
 
 resource "aws_security_group" "load_balancer_sg" {
+  vpc_id = module.vpc.vpc_id
   ingress = [{
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -187,12 +253,20 @@ resource "aws_security_group" "load_balancer_sg" {
   }]
 
 }
+
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = module.ec2_instances.id[0]
+  depends_on = [
+    aws_lb_target_group.target_group
+  ]
+}
 resource "aws_lb_target_group" "target_group" {
   name        = "freqtrade-tg"
   port        = "8080"
   protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_default_vpc.default_vpc.id
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
   health_check {
     matcher  = "200"
     path     = "/api/v1/ping"
@@ -202,7 +276,7 @@ resource "aws_lb_target_group" "target_group" {
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_alb.application_load_balancer.arn
+  load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = "8080"
   protocol          = "HTTP"
 
